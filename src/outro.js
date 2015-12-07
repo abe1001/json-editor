6b303aca7ed7d7f9e9dda43e1f375273cf39ee37
abe <abe@ambita.com>
2015-12-07 09:35:56 +0100

    Added module handling

--- src/outro.js
+++ src/outro.js
@@ -1,2 +1,8 @@
+/*global module*/
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = JSONEditor;
+}
+if (typeof window !== 'undefined') {
   window.JSONEditor = JSONEditor;
+}
 })();
